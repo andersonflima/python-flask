08b9d5286f8ca59b0233c0e71ff186a047c6c47a
Anderson F Lima <andersonfaustinolima@gmail.com>
2021-01-11 00:39:27 -0300

    added blueprint import

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask
-
+from flask import Flask, Blueprint
 
 app = Flask(__name__)
+urls = Blueprint('urls', __name__)
 
-@app.route('/', methods=['GET'])
+@urls.route('/')
 def hello_world():
     return 'Hello World!'
+    
+app.register_blueprint(urls)
 
 if __name__=='__main__':
     app.run('localhost',port=3000)
